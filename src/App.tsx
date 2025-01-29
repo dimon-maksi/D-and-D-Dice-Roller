e848743d0ce281d1b9ee4c5a27bbcf0ae923920d
FALLBACK\maksy <maksymenko.ds.3@gmail.com>
2025-01-30 00:57:03 +0200

    fix: adaptive layout lg -> xl

--- src/App.tsx
+++ src/App.tsx
@@ -19,14 +19,14 @@ function App() {
 
   return (
     <div className="bg-primary p-4 h-screen flex gap-4 overflow-hidden">
-      <div className="column hidden lg:flex lg:w-[15%]">
+      <div className="column hidden xl:flex xl:w-[15%]">
         <History rolls={rolls} />
       </div>
-      <div className="column lg:w-[70%] w-2/3 text-center">
+      <div className="column xl:w-[70%] w-2/3 text-center">
         <h1 className="text-4xl lg:text-6xl font-bold py-4">D&D Dice Roller</h1>
         <Dices dices={dices}/>
       </div>
-      <div className="column flex lg:w-[15%] w-1/3">
+      <div className="column flex xl:w-[15%] w-1/3">
         <Options setRolls={setRolls} setDices={setDices}/>
       </div>
     </div>
